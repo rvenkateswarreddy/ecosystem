b1734df806b8cb9bf649b3726f0dd670ebc377a7
R.venkateswar reddy <rvenkateswarreddy12345@gmail.com>
2024-03-24 11:37:20 +0530

    dfod

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -45,7 +45,9 @@ const Navbar = () => {
           >
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9c-1.63 0-3-1.37-3-3s1.37-3 3-3 3 1.37 3 3-1.37 3-3 3z" />
           </svg>
-          <Link className="font-semibold text-xl tracking-tight">EcoTree</Link>
+          <Link className="font-semibold text-xl tracking-tight">
+            Greenvestements
+          </Link>
         </div>
         <div className="flex items-center justify-start flex-grow">
           <div className="ml-12 flex gap-9 items-center space-x-4">
